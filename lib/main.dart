adb57d5281e3933966be753c26fa65d6885ee896
Vanesa Nellie <70554646+vanesanellie@users.noreply.github.com>
2021-02-17 03:17:49 +0700

    l

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pokepoke.dart';
+import "package:http/http.dart" as http;
 
 void main() {
   runApp(new MaterialApp(
@@ -17,36 +21,91 @@ class HomePage extends StatefulWidget {
   
 class HomePageState extends State<HomePage> {
 
+  var url = "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
   
+  PokeUhuy pokeUhuy; 
 
   @override
   void initState(){
     super.initState();
     fetchData();
+    print("2nd work");
   }
 
-    fetchData() async {}
+    fetchData() async {
+      var res = await http.get(url);
+      var decodedJson = jsonDecode(res.body);
+      pokeUhuy = PokeUhuy.fromJson(decodedJson);
+      print(pokeUhuy.toJson());
+    setState(() {});
+    }
 
   @override
   Widget build(BuildContext context) {
-    var scaffold = Scaffold(
+    var map = pokeUhuy.pokemon.map((poke) => Card());
+    var scaffold3 = Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black87,
         title: Text("Poke App"),
         ),
-        body: Center(
-          child: Text("Hello from me ^v^"),
-        ), 
+        body: pokeUhuy == null
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : GridView.count(
+              crossAxisCount: 2,
+              children: pokeUhuy.pokemon
+                  .map((poke) => Padding(
+                        padding: const EdgeInsets.all(2.0),
+                        child: InkWell(
+                          onTap: () {
+                            var materialPageRoute = MaterialPageRoute(
+                                    builder: (context) => PokeDetail(
+                                          pokemon: poke,
+                                        ));
+                            Navigator.push(
+                                context,
+                                materialPageRoute);
+                          },
+                          child: Hero(
+                            tag: poke.img,
+                            child: Card(
+                              child: Column(
+                                mainAxisSize: MainAxisSize.max,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: <Widget>[
+                                  Container(
+                                    height: MediaQuery.of(context).size.height *
+                                        0.4,
+                                    width:
+                                        MediaQuery.of(context).size.width * 0.2,
+                                    decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                            fit: BoxFit.cover,
+                                            image: NetworkImage(poke.img))),
+                                  ),
+                                  Text(
+                                    poke.name,
+                                    style: TextStyle(
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ))
+                  .toList(),
+            ),
         drawer: Drawer(),
         floatingActionButton: FloatingActionButton(
             onPressed: () {},
-            backgroundColor: Colors.blueGrey,
+            backgroundColor: Colors.grey,
             child: Icon(Icons.refresh)
             ),
     );
-    return scaffold;
-    
-
-    
   }
 }
